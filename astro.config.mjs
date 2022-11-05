ac9cc5df3142755da5e4eb2f298a5deefed2c080
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-11-05 11:14:36 +0900

    svelte導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@
 
 import { defineConfig } from "astro/config"
 import { resolve } from "node:path"
-
+import svelte from "@astrojs/svelte"
 const __dirname = new URL(".", import.meta.url).pathname
 
-// https://astro.build/config
 export default defineConfig(
   /** @type {import('astro').AstroUserConfig} */
   {
@@ -16,5 +15,6 @@ export default defineConfig(
         },
       },
     },
+    integrations: [svelte()],
   },
 )
