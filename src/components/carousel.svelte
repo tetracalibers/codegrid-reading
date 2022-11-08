4ab990271f0e6ea1ed40e0c4c4b26f12c18a37b7
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-11-08 17:01:32 +0900

    [Carousel] slideのレイアウトとindicatorのスタイリング

--- src/components/carousel.svelte
+++ src/components/carousel.svelte
@@ -15,53 +15,82 @@
     }
   }
 
+  const calcMoveOffset = (nowIdx: number) => {
+    // width: 100%; gap: 5%;
+    return nowIdx * -100 + nowIdx * -5
+  }
+
   export let count: number
   let calcMovedIdx = counter(count)
+  let currIdx = 0
   let offset = 0
+
+  const move = () => {
+    offset = calcMoveOffset(currIdx)
+  }
 
   const onClickNextButton = (e: MouseEvent | TouchEvent) => {
     e.preventDefault()
-    offset = calcMovedIdx("next") * -100
+    currIdx = calcMovedIdx("next")
+    move()
   }
 
   const onClickPrevButton = (e: MouseEvent | TouchEvent) => {
     e.preventDefault()
-    offset = calcMovedIdx("prev") * -100
+    currIdx = calcMovedIdx("prev")
+    move()
   }
 
   const onClickIndicator = (e: MouseEvent | TouchEvent, idx: number) => {
     e.preventDefault()
-    offset = calcMovedIdx(idx) * -100
+    currIdx = calcMovedIdx(idx)
+    move()
   }
 </script>
 
 <div class="carousel">
-  <div class="carousel__slide-list" style={`--slide-offset: ${offset}%`}>
-    <slot />
+  <div class="carousel-visible">
+    <div class="carousel-slide" style={`--slide-offset: ${offset}%`}>
+      <slot />
+    </div>
   </div>
+  <div class="carousel-buttons">
+    <button on:click={onClickPrevButton}>Prev</button>
+    <button on:click={onClickNextButton}>Next</button>
+  </div>
+  <ul class="carousel-indicator">
+    {#each [...new Array(count)] as _, i}
+      <li class="carousel-indicator__item">
+        <button
+          class={[
+            `carousel-indicator__button`,
+            i === currIdx ? "--active" : "",
+          ].join(" ")}
+          on:click={e => onClickIndicator(e, i)}
+        >
+          <span class="visually-hidden">{i + 1}</span>
+        </button>
+      </li>
+    {/each}
+  </ul>
 </div>
-<button on:click={onClickPrevButton}>Prev</button>
-<button on:click={onClickNextButton}>Next</button>
-<ul>
-  {#each [...new Array(count)] as _, i}
-    <li>
-      <button on:click={e => onClickIndicator(e, i)}>
-        {i + 1}
-      </button>
-    </li>
-  {/each}
-</ul>
 
 <style>
   /** 見える範囲 */
   .carousel {
   }
 
+  .carousel-visible {
+    overflow: hidden;
+    padding: 2.5%;
+  }
+
   /** スライドするアイテム全体 */
-  .carousel__slide-list {
+  .carousel-slide {
     display: flex;
     flex-wrap: nowrap;
     width: 100%;
+    gap: 5%;
     /* トランジション周りの指定を追加 */
     transform: translateX(var(--slide-offset, 0));
     transition-property: transform;
@@ -69,9 +98,56 @@
     transition-timing-function: ease;
   }
 
-  :global(.carousel__slide-list-item) {
+  :global(.carousel-slide__item) {
     flex-grow: 0;
     flex-shrink: 0;
     flex-basis: 100%;
+  }
+
+  .carousel-indicator {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .carousel-indicator__item {
+    /** heightを子要素にfitさせる */
+    display: flex;
+  }
+
+  .carousel-indicator__button {
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    appearance: none;
+    border: none;
+    padding: 0;
+    background-color: #eef1ff;
+  }
+
+  .carousel-indicator__button:hover {
+    cursor: pointer;
+  }
+
+  .carousel-indicator__button:hover,
+  .carousel-indicator__button:focus,
+  .carousel-indicator__button:active,
+  .carousel-indicator__button.--active {
+    background-color: #b1afff;
+  }
+
+  .visually-hidden {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
   }
 </style>
